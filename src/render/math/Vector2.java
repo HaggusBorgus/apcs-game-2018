d1d2c1a1f7c8e6392ca5e3da20fb5f117231b048
Dezz Meister <dezzy3359@gmail.com>
2018-06-07 21:35:22 -0400

    Vector2 added

---
+++ src/render/math/Vector2.java
@@ -0,0 +1,40 @@
+package render.math;
+
+public class Vector2 {
+	public float x;
+	public float y;
+	public float length = -1;
+	
+	public Vector2(float _x, float _y) {
+		x = _x;
+		y = _y;
+	}
+	
+	public Vector2 add(Vector2 v) {
+		return new Vector2(v.x + x, v.y + y);
+	}
+	
+	public void updateLength() {
+		length = (float)Math.sqrt((x * x) + (y * y));
+	}
+	
+	public Vector2 subtract(Vector2 v) {
+		return new Vector2(x - v.x, y - v.y);
+	}
+	
+	/**
+	 * Returns the Euclidean distance from one Vector2 to another. Uses <code>Math.sqrt()</code>.
+	 * 
+	 * @param _v0 first Vector2
+	 * @param _v1 second Vector2
+	 * @return Euclidean distance between both vectors
+	 */
+	public static float distance(Vector2 _v0, Vector2 _v1) {
+		return (float) Math.sqrt(((_v0.x - _v1.x) * (_v0.x - _v1.x)) + ((_v0.y - _v1.y) * (_v0.y - _v1.y)));
+	}
+	
+	@Override
+	public String toString() {
+		return "("+x+","+y+")";
+	}
+}
